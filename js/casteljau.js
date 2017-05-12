8d3783a7b3c542a5bb85a2dcd1cd745588cbe717
rcrs3 <rcrs3@cin.ufpe.br>
2017-05-12 14:27:14 -0300

    Adicionado elevação do grau da curva

--- js/casteljau.js
+++ js/casteljau.js
@@ -3,10 +3,16 @@ var points = [];
 var showPoint = true;
 var showPolygon = true;
 var showCurve = true;
-var evaluations = 100;
+var evaluations = 500;
 var pathsPolygonG = [];
 var pathsCurveG = [];
+var elev = true;
 //var paths = [];
+
+stage.on('message:ElevateDegree', function(data) {
+	elev = data.data;
+	elevate();
+})
 
 stage.on('message:receiveEvaluation', function(data) {
 	evaluations = data.data;
@@ -28,6 +34,50 @@ stage.on('message:receiveCurveBox', function(data) {
 	showCurve = data.data;
 	getDraw();
 })
+
+function elevate(){
+	var n = points.length;
+	var newPoints = [];
+	var newCircles = [];
+
+	for (var i = 0; i <= n; i++) {
+		if(i == n) {
+			newPoints.push (new Point(points[i-1].x,points[i-1].y));
+		}
+		else if(i == 0) {
+			newPoints.push (new Point(points[i].x,points[i].y));
+		}
+		else {
+			newPoints.push(new Point(
+				((i/(n+1))*points[i-1].x + (1 - (i/(n+1)))*points[i].x),
+				((i/(n+1))*points[i-1].y + (1 - (i/(n+1)))*points[i].y)));
+		}
+		newCircles.push (new Circle(newPoints[i].x, newPoints[i].y, 6)
+								.attr('fillColor', 'gray'));
+	}
+	circles = [];
+	points = [];
+
+	for (var k=0; k<newPoints.length; k++) {
+		circles.push(new Circle(newCircles[k].attr('x'), newCircles[k].attr('y'), 6)
+														.attr('fillColor', 'gray').on('drag', function(evt){
+      this.attr({x: evt.x, y:evt.y});
+    }));
+		points.push (new Point(newPoints[k].x,newPoints[k].y));
+
+		var pathsPolygon = [];
+		for(var i = 0; i < circles.length-1; i++) {
+			pathsPolygon.push(new Path([['moveTo', circles[i].attr('x'), circles[i].attr('y')],
+			 					['lineTo', circles[i+1].attr('x'), circles[i+1].attr('y')]
+			 					]).stroke('gray', 1));
+		}
+		pathsPolygonG = pathsPolygon;
+		calculateCasteljau();
+		getDraw();
+	}
+
+}
+
 
 function interpolation(t, p1, p2){
 	return new Point(t*p1.x + (1-t)*p2.x, t*p1.y + (1-t)*p2.y);
@@ -78,27 +128,24 @@ function calculateCasteljau() {
 	pathsCurveG = [];
 	if(points.length > 2) {
 		var curve = getCasteljau();
-		//console.log("oi");
 		pathsCurveG.push(curve);
 	}
 }
 
 stage.on('click', function(e) {
-	var delet = false;
+	var exist = false;
 	for(var i = 0; i < circles.length; i++) {
 		if(e.target == circles[i]) {
-			circles.splice(i, 1);
-			points.splice(i, 1);
-			delet = true;
+			exist = true;
 		}
 	}
-	if(!delet) {
-		circles.push(new Circle(e.x, e.y, 4)
-						.addTo(stage)
-						.attr('fillColor', 'gray'));
+	if(!exist) {
+		circles.push(new Circle(e.x, e.y, 6).attr('fillColor', 'gray').on('drag', function(evt){
+      this.attr({x: evt.x, y:evt.y});
+    }));
 		points.push(new Point(e.x, e.y));
 	}
-	if(circles.length > 1) {
+	if(circles.length > 0) {
 		var pathsPolygon = [];
 		for(var i = 0; i < circles.length-1; i++) {
 			pathsPolygon.push(new Path([['moveTo', circles[i].attr('x'), circles[i].attr('y')],
@@ -121,4 +168,23 @@ stage.on('pointerup', function(e) {
 			points[i].y = e.y;
 		}
 	}
+})
+
+stage.on('doubleclick', function(e) {
+	for(var i = 0; i < circles.length; i++) {
+		if(e.target == circles[i]) {
+			circles.splice(i, 1);
+			points.splice(i, 1);
+		}
+	}
+	var pathsPolygon = [];
+	for(var i = 0; i < circles.length-1; i++) {
+		pathsPolygon.push(new Path([['moveTo', circles[i].attr('x'), circles[i].attr('y')],
+		 					['lineTo', circles[i+1].attr('x'), circles[i+1].attr('y')]
+		 					]).stroke('gray', 1));
+	}
+
+	pathsPolygonG = pathsPolygon;
+	calculateCasteljau();
+	getDraw();
 })
